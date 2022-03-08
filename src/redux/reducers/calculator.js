41de7997f88d9a5456e90765ee3f8da2bb0fa99a
Chimoha31 <mokochii1108@gmail.com>
2022-03-07 18:49:39 -0800

    reducer

--- src/redux/reducers/calculator.js
+++ src/redux/reducers/calculator.js
@@ -25,24 +25,135 @@ const calculator = (state = initialAppState, action) => {
         showingResult: false,
       };
 
-    case PLUS: 
-    if(state.calculate === true) {
-      return {
-        ...state,
-        inputValue: 0,
-        operator: '+',
-        resultValue: state.resultValue + state.inputValue,
-        showingResult: true,
-      };
-    }else{
-      return{
-        ...state,
-        inputValue: 0,
-        operator: '+',
-        calculate: true,
-        resultValue: state.inputValue,
-        showingResult: true,
+    case PLUS:
+      if (state.calculate === true) {
+        return {
+          ...state,
+          inputValue: 0,
+          operator: "+",
+          resultValue: state.resultValue + state.inputValue,
+          showingResult: true,
+        };
+      } else {
+        return {
+          ...state,
+          inputValue: 0,
+          operator: "+",
+          calculate: true,
+          resultValue: state.inputValue,
+          showingResult: true,
+        };
       }
-    }
+
+    case MINUS:
+      if (state.calculate === true) {
+        return {
+          ...state,
+          inputValue: 0,
+          operator: "-",
+          resultValue: state.resultValue + state.inputValue,
+          showingResult: true,
+        };
+      } else {
+        return {
+          ...state,
+          inputValue: 0,
+          operator: "-",
+          calculate: true,
+          resultValue: state.inputValue,
+          showingResult: true,
+        };
+      }
+
+    case MULTIPLY:
+      if (state.calculate === true) {
+        return {
+          ...state,
+          inputValue: 0,
+          operator: "*",
+          resultValue: state.resultValue + state.inputValue,
+          showingResult: true,
+        };
+      } else {
+        return {
+          ...state,
+          inputValue: 0,
+          operator: "*",
+          calculate: true,
+          resultValue: state.inputValue,
+          showingResult: true,
+        };
+      }
+
+    case DEVIDE:
+      if (state.calculate === true) {
+        return {
+          ...state,
+          inputValue: 0,
+          operator: "/",
+          resultValue: state.resultValue + state.inputValue,
+          showingResult: true,
+        };
+      } else {
+        return {
+          ...state,
+          inputValue: 0,
+          operator: "/",
+          calculate: true,
+          resultValue: state.inputValue,
+          showingResult: true,
+        };
+      }
+
+    case CLEAR:
+      return {
+        inputValue: 0,
+        operator: "",
+        calculate: false,
+        resultValue: 0,
+        showingResult: false,
+      };
+
+    case EQUAL:
+      switch (state.operator) {
+        case "+":
+          return {
+            inputValue: state.resultValue + state.inputValue,
+            operator: "",
+            calculate: false,
+            resultValue: state.resultValue + state.inputValue,
+            showingResult: true,
+          };
+        case "-":
+          return {
+            inputValue: state.resultValue - state.inputValue,
+            operator: "",
+            calculate: false,
+            resultValue: state.resultValue - state.inputValue,
+            showingResult: true,
+          };
+        case "*":
+          return {
+            inputValue: state.resultValue * state.inputValue,
+            operator: "",
+            calculate: false,
+            resultValue: state.resultValue * state.inputValue,
+            showingResult: true,
+          };
+        case "/":
+          return {
+            inputValue: state.resultValue / state.inputValue,
+            operator: "",
+            calculate: false,
+            resultValue: state.resultValue / state.inputValue,
+            showingResult: true,
+          };
+        default:
+          return state;
+      }
+    default:
+      return state;
   }
 };
+
+export default calculator;
